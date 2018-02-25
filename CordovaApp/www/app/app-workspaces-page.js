e69037777ba793c2813b3d5fc1ff8987e923b1a3
Joseph Francis <joefran@us.ibm.com>
2018-02-25 10:18:37 -0500

    Updated WS page to lazy load templates on first activate

--- CordovaApp/www/app/app-workspaces-page.js
+++ CordovaApp/www/app/app-workspaces-page.js
@@ -29,14 +29,25 @@ License: MIT
     //--- Start with a ase SitePage component
     var ThisPage = new SiteMod.SitePage(thisSiteSpecs);
 
+    ThisPage._onInit = function(theApp) {
+        ThisPage._svg = theApp.getComponent("plugin:SvgControls");
+        ThisPage._om = theApp.getComponent("plugin:ObjectManager");
+
+         
+    }
+
     //--- Hook into the app lifecycle as needed
     ThisPage._onFirstActivate = function() {
-        
-        var tmpContext = {}
-        ThisPage.loadRegion('center', ThisApp.renderTemplate(thisSiteSpecs.pageActionPrefix + ':page-body', tmpContext));
-        ThisPage.loadRegion('south', ThisApp.renderTemplate(thisSiteSpecs.pageActionPrefix + ':page-footer', tmpContext));
-//        ThisPage.loadRegion('center', ThisApp.renderTemplate(thisSiteSpecs.pageActionPrefix + ':page-body', tmpContext));
-//        ThisPage.loadRegion('south', ThisApp.renderTemplate(thisSiteSpecs.pageActionPrefix + ':page-footer', tmpContext));
+        ThisPage._om.getObjects('[html]:app-tpl/app-workspaces-page', ['page-body.html','page-footer.html']).then(function (theDocs) {
+            for( var aKey in theDocs ){
+                var tmpTN = aKey.replace('.html','');
+                tmpTN = thisSiteSpecs.pageActionPrefix + ':' + tmpTN;
+                ThisApp.tplHandlebars[tmpTN] = Handlebars.compile(theDocs[aKey]); 
+			}
+            var tmpContext = {}
+            ThisPage.loadRegion('center', ThisApp.renderTemplate(thisSiteSpecs.pageActionPrefix + ':page-body', tmpContext));
+            ThisPage.loadRegion('south', ThisApp.renderTemplate(thisSiteSpecs.pageActionPrefix + ':page-footer', tmpContext));
+        });
     }
 
     //--- Implement this apge
